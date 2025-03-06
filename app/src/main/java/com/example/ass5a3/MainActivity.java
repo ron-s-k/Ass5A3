c666d7c90b66a83388e689172ca9c5d74ee6c59a
ron-s-k <ronitk8484@gmail.com>
2025-03-06 10:13:49 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ass5a3/MainActivity.java
@@ -0,0 +1,150 @@
+package com.example.ass5a3;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText etEmp_name, etEmp_address, etEmp_phone, etEmp_salary, etDep_id, etDep_name, etDep_loc;
+    private Button btnInsert, btnShow, btnShowDep, btnInsertDep;
+    private myDatabase dbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        etEmp_name = findViewById(R.id.name);
+        etEmp_address = findViewById(R.id.address);
+        etEmp_phone = findViewById(R.id.phoneno);
+        btnInsert = findViewById(R.id.insertButton);
+        btnShow = findViewById(R.id.showButton);
+        etEmp_salary = findViewById(R.id.salary);
+        etDep_id = findViewById(R.id.dep_id);
+        etDep_name = findViewById(R.id.dep_name);
+        etDep_loc = findViewById(R.id.location);
+        btnShowDep = findViewById(R.id.btnShowDep);
+        btnInsertDep = findViewById(R.id.insertDep);
+
+        // Initialize Database Helper
+        dbHelper = new myDatabase(this);
+
+        // Insert employee button click event
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get user inputs
+                String name = etEmp_name.getText().toString();
+                String address = etEmp_address.getText().toString();
+                String phone = etEmp_phone.getText().toString();
+                String salary = etEmp_salary.getText().toString();
+                String dep_id = etDep_id.getText().toString();
+
+                int dep_id_int = Integer.parseInt(dep_id);
+                // Insert employee data into the database
+                dbHelper.insertDataEmp(name, address, phone, salary, dep_id_int);
+
+                // Clear input fields after insertion
+                etEmp_name.setText("");
+                etEmp_address.setText("");
+                etEmp_phone.setText("");
+                etEmp_salary.setText("");
+                etDep_id.setText("");
+            }
+        });
+
+        // Insert department button click event
+        btnInsertDep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String dep_id = etDep_id.getText().toString();
+                int dep_id_int = Integer.parseInt(dep_id);
+                String dep_name = etDep_name.getText().toString();
+                String dep_loc = etDep_loc.getText().toString();
+
+                // Insert department data into the database
+                dbHelper.insertDataDep(dep_id_int, dep_name, dep_loc);
+
+                // Clear input fields after insertion
+                etDep_id.setText("");
+                etDep_name.setText("");
+                etDep_loc.setText("");
+            }
+        });
+
+        // Show employee data button click event
+        btnShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Cursor cursor = dbHelper.showDataEmp();
+                if (cursor.getCount() == 0) {
+                    showMessage("Error", "No Data Found");
+                } else {
+                    StringBuffer buffer = new StringBuffer();
+                    cursor.moveToFirst();
+                    do {
+                        String id = cursor.getString(0); // Get the ID column
+                        String name = cursor.getString(1); // Get the NAME column
+                        String address = cursor.getString(2); // Get the ADDRESS column
+                        String phone = cursor.getString(3); // Get the PHONE column
+                        String salary = cursor.getString(4); // Get the SALARY column
+                        String dep_id = cursor.getString(5); // Get the DEPARTMENT column
+
+                        buffer.append("Emp ID = " + id);
+                        buffer.append("\nEmp Name = " + name);
+                        buffer.append("\nEmp Address = " + address);
+                        buffer.append("\nEmp Phone = " + phone);
+                        buffer.append("\nEmp Salary = " + salary);
+                        buffer.append("\nEmp Department = " + dep_id);
+                        buffer.append("\n\n");
+                    } while (cursor.moveToNext());
+
+                    showMessage("Data", buffer.toString());
+                }
+            }
+        });
+
+        // Show department data button click event
+        btnShowDep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Cursor cursor = dbHelper.showDataDep();
+                if (cursor.getCount() == 0) {
+                    showMessage("Error", "No Data Found");
+                } else {
+                    StringBuffer buffer = new StringBuffer();
+                    cursor.moveToFirst();
+                    do {
+                        String id = cursor.getString(0); // Get the ID column
+                        String name = cursor.getString(1); // Get the NAME column
+                        String location = cursor.getString(2); // Get the LOCATION column
+
+                        buffer.append("Dep ID = " + id);
+                        buffer.append("\nDep Name = " + name);
+                        buffer.append("\nDep Location = " + location);
+                        buffer.append("\n\n");
+                    } while (cursor.moveToNext());
+
+                    showMessage("Data", buffer.toString());
+                }
+            }
+        });
+    }
+
+    // Method to show a message in a dialog
+    private void showMessage(String title, String info) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(info);
+        builder.show();
+    }
+}
---
+++ app/src/main/java/com/example/ass5a3/myDatabase.java
@@ -0,0 +1,76 @@
+package com.example.ass5a3;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class myDatabase extends SQLiteOpenHelper {
+
+    public static final String COMPANY_DATABASE="employee.db";
+    public static final String EMPLOYEE_TABLE = "employee";
+    public static final String E_COLUMN_1 = "EMP_NO";
+    public static final String E_COLUMN_2 = "NAME";
+    public static final String E_COLUMN_3 = "ADDRESS";
+    public static final String E_COLUMN_4 = "PHONE";
+    public static final String E_COLUMN_5 = "SALARY";
+    public static final String E_COLUMN_6 = "DEPARTMENT";
+
+    public static final String DEPARTMENT_TABLE = "department";
+    public static final String D_COLUMN_1 = "ID";
+    public static final String D_COLUMN_2 = "NAME";
+    public static final String D_COLUMN_3 = "LOCATION";
+
+    public myDatabase( Context context) {
+        super(context,COMPANY_DATABASE,null,1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE "+EMPLOYEE_TABLE+"(ID INTEGER PRIMARY KEY AUTOINCREMENT,NAME TEXT,ADDRESS TEXT,PHONE TEXT,SALARY TEXT,DEPARTMENT INTEGER, FOREIGN KEY(DEPARTMENT) REFERENCES DEPARTMENT(ID))");
+        db.execSQL("CREATE TABLE "+DEPARTMENT_TABLE+"(ID INTEGER PRIMARY KEY ,NAME TEXT,LOCATION TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public void insertDataEmp(String name,String address,String phone,String salary,int dep_id){
+        Log.d("myDatabase","insertDataEmp");
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(E_COLUMN_2,name);
+        cv.put(E_COLUMN_3,address);
+        cv.put(E_COLUMN_4,phone);
+        cv.put(E_COLUMN_5,salary);
+        cv.put(E_COLUMN_6, dep_id);
+        db.insert(EMPLOYEE_TABLE,null,cv);
+    }
+
+    public void insertDataDep(int dep_id, String dep_name, String dep_loc){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(D_COLUMN_1,dep_id);
+        cv.put(D_COLUMN_2,dep_name);
+        cv.put(D_COLUMN_3,dep_loc);
+        db.insert(DEPARTMENT_TABLE,null,cv);
+    }
+
+    public Cursor showDataEmp(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM "+EMPLOYEE_TABLE,null);
+
+        return cursor;
+    }
+
+    public Cursor showDataDep() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM " + DEPARTMENT_TABLE, null);
+
+        return cursor;
+    }
+
+}
